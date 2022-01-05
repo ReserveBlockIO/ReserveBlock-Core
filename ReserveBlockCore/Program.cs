bc2c595377ba0d85d2664c5693ce3f84d207cae4
Mathis <20599614+mathis1337@users.noreply.github.com>
2022-01-04 21:51:17 -0600

    Skeleton base project commit to stage for upcoming blockchain assets

---
+++ ReserveBlockCore/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Starting up Reserve Block Wallet");
+
+Thread.Sleep(1000);
+
+Console.WriteLine("Wallet Started. Awaiting Command...");
+
+while (true)
+{
+    var command = Console.ReadLine();
+
+    if (command != "" || command != null)
+    {
+        var commandResult = ReserveBlockCore.Utilties.CommandUtility.ProcessCommand(command);
+
+        if (commandResult == "_EXIT")
+        {
+            Console.WriteLine("Closing and Exiting Wallet Application.");
+            Environment.Exit(0);
+        }
+
+        Console.WriteLine(commandResult);
+    }
+    else
+    {
+        Console.WriteLine("Please enter a command...");
+    }
+
+}
---
+++ ReserveBlockCore/Utilities/CommandUtility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReserveBlockCore.Utilties
+{
+    internal class CommandUtility
+    {
+        internal static string ProcessCommand(string command)
+        {
+            var commandResult = string.Empty;
+
+            switch (command)
+            {
+                case "/help":
+                    commandResult = "Help Command List Goes Here...";
+                    break;
+                case "/exit":
+                    commandResult = "_EXIT";
+                    break;
+                default:
+                    commandResult = "Not a recognized command. Please Try Again...";
+                    break;
+            }
+
+            return commandResult;
+
+        }
+    }
+}
