f23e2a13ef10ff0e3c8c3442c8bb50391c78ed4d
Mathis <20599614+mathis1337@users.noreply.github.com>
2022-02-05 13:30:28 -0600

    Add base58 and HexByte Utils

---
+++ ReserveBlockCore/Utilities/Base58Utility.cs
@@ -0,0 +1,56 @@
+﻿using System.Numerics;
+
+namespace ReserveBlockCore.Utilities
+{
+    public class Base58Utility
+    {
+		public static string Base58Encode(byte[] array)
+		{
+			const string ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+			string retString = string.Empty;
+			BigInteger encodeSize = ALPHABET.Length;
+			BigInteger arrayToInt = 0;
+
+			for (int i = 0; i < array.Length; ++i)
+			{
+				arrayToInt = arrayToInt * 256 + array[i];
+			}
+
+			while (arrayToInt > 0)
+			{
+				int rem = (int)(arrayToInt % encodeSize);
+				arrayToInt /= encodeSize;
+				retString = ALPHABET[rem] + retString;
+			}
+
+			for (int i = 0; i < array.Length && array[i] == 0; ++i)
+				retString = ALPHABET[0] + retString;
+			return retString;
+		}
+
+		public static byte[] Base58Decode(string s)
+		{
+			const string ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+			// Decode Base58 string to BigInteger 
+			BigInteger intData = 0;
+			for (int i = 0; i < s.Length; i++)
+			{
+				int digit = ALPHABET.IndexOf(s[i]); //Slow
+				if (digit < 0)
+					throw new FormatException(string.Format("Invalid Base58 character `{0}` at position {1}", s[i], i));
+				intData = intData * 58 + digit;
+			}
+
+			// Encode BigInteger to byte[]
+			// Leading zero bytes get encoded as leading `R` characters
+			int leadingZeroCount = s.TakeWhile(c => c == 'R').Count();
+			//var leadingZeros = Enumerable.Repeat((byte)0, leadingZeroCount);
+			var bytesWithoutLeadingZeros =
+				intData.ToByteArray()
+				.Reverse()// to big endian
+				.SkipWhile(b => b == 0);//strip sign byte
+			var result = bytesWithoutLeadingZeros.ToArray();
+			return result;
+		}
+	}
+}
---
+++ ReserveBlockCore/Utilities/HexByteUtility.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace ReserveBlockCore.Utilities
+{
+    public class HexByteUtility
+    {
+		public static string ByteToHex(byte[] pubkey)
+		{
+			return Convert.ToHexString(pubkey).ToLower();
+		}
+		public static byte[] HexToByte(string HexString)
+		{
+			if (HexString.Length % 2 != 0)
+				throw new Exception("Invalid HEX");
+			byte[] retArray = new byte[HexString.Length / 2];
+			for (int i = 0; i < retArray.Length; ++i)
+			{
+				retArray[i] = byte.Parse(HexString.Substring(i * 2, 2), NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+			}
+			return retArray;
+		}
+	}
+}
