2c85a2b9e563ab19fce076ebd055a29177552dc9
Mathis <20599614+mathis1337@users.noreply.github.com>
2022-02-08 03:16:40 -0600

    Create FeeCalcService.cs

---
+++ ReserveBlockCore/Services/FeeCalcService.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using ReserveBlockCore.Models;
+
+namespace ReserveBlockCore.Services
+{
+    public class FeeCalcService
+    {
+        public static decimal CalculateTXFee(Transaction tx)
+        {
+            var txFee = new decimal();
+            var baseFeeMultiplier = 0.00001M;
+            var kb = 1024.0M;
+
+            var txSize = JsonConvert.SerializeObject(tx).Length;
+            
+            txFee = (txSize / kb) * baseFeeMultiplier; 
+
+            return txFee;
+        }
+
+        public static void CalculateDataFee()
+        {
+            //calc for data fee 
+        }
+    }
+}
