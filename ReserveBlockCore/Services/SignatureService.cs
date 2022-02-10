570cf04ef7085f9dcc8cad23c647ddf1e883cd37
Mathis <20599614+mathis1337@users.noreply.github.com>
2022-02-10 16:44:20 -0600

    Created new signature service

---
+++ ReserveBlockCore/Services/SignatureService.cs
@@ -0,0 +1,51 @@
+﻿using ReserveBlockCore.Data;
+using ReserveBlockCore.EllipticCurve;
+using ReserveBlockCore.Utilities;
+
+namespace ReserveBlockCore.Services
+{
+    public static class SignatureService
+    {
+        public static string CreateSignature(string message, PrivateKey PrivKey, string pubKey)
+        {
+
+            Signature signature = Ecdsa.sign(message, PrivKey);
+            var sigBase64 = signature.toBase64();
+            var pubKeyEncoded = Base58Utility.Base58Encode(HexByteUtility.HexToByte(pubKey.Remove(0, 2)));
+            var sigScript = sigBase64 + "." + pubKeyEncoded;
+
+            //validate new signature
+            var sigScriptArray = sigScript.Split('.', 2);
+            var pubKeyDecoded = HexByteUtility.ByteToHex(Base58Utility.Base58Decode(sigScriptArray[1]));
+            var pubKeyByte = HexByteUtility.HexToByte(pubKeyDecoded);
+            var publicKey = PublicKey.fromString(pubKeyByte);
+            var verifyCheck = Ecdsa.verify(message, Signature.fromBase64(sigScriptArray[0]), publicKey);
+
+            if (verifyCheck != true)
+                return "ERROR";
+            return sigScript;
+        }
+
+        public static bool VerifySignature(string address, string message, string sigScript)
+        {
+            var sigScriptArray = sigScript.Split('.', 2);
+            var pubKeyDecoded = HexByteUtility.ByteToHex(Base58Utility.Base58Decode(sigScriptArray[1]));
+            var pubKeyByte = HexByteUtility.HexToByte(pubKeyDecoded);
+            var publicKey = PublicKey.fromString(pubKeyByte);
+
+            var _PublicKey = "04" + ByteToHex(publicKey.toString());
+            var _Address = AccountData.GetHumanAddress(_PublicKey);
+
+            if(address != _Address)
+            {
+                return false;
+            }
+
+            return Ecdsa.verify(message, Signature.fromBase64(sigScriptArray[0]), publicKey);
+        }
+        private static string ByteToHex(byte[] pubkey)
+        {
+            return Convert.ToHexString(pubkey).ToLower();
+        }
+    }
+}
