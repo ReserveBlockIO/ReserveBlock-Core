c92e4d0af7b934a74792c4ac0d0ab6ccb3e4deda
Mathis <20599614+mathis1337@users.noreply.github.com>
2022-05-09 15:32:30 -0500

    Smart Contract Asset Models

---
+++ ReserveBlockCore/Models/SmartContracts/SmartContractAsset.cs
@@ -0,0 +1,11 @@
+﻿namespace ReserveBlockCore.Models.SmartContracts
+{
+    public class SmartContractAsset
+    {
+        public Guid AssetId { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Extension { get; set; }
+        public long FileSize { get; set; }
+    }
+}
--- ReserveBlockCore/Models/SmartContracts/SmartContractMain.cs
+++ ReserveBlockCore/Models/SmartContracts/SmartContractMain.cs
@@ -1,11 +1,11 @@
-﻿namespace ReserveBlockCore.Models
+﻿namespace ReserveBlockCore.Models.SmartContracts
 {
     public class SmartContractMain
     {
         public string Name { get; set; } //User Defined
         public string Description { get; set; } //User Defined
         public string Address { get; set; } //User Defined
-        public string AssetId { get; set; } //User Defined - But not now
+        public SmartContractAsset SmartContractAsset { get; set; } 
         public bool IsPublic { get; set; } //System Set
         public Guid SmartContractUID { get; set; }//System Set
         public string Signature { get; set; }//System Set
