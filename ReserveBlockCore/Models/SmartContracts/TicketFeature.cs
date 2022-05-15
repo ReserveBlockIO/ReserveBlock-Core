e4925099790ea4354f0f841bc9c1cea0128e1f74
Mathis <20599614+mathis1337@users.noreply.github.com>
2022-05-14 22:53:30 -0500

    Update TicketFeature.cs

--- ReserveBlockCore/Models/SmartContracts/TicketFeature.cs
+++ ReserveBlockCore/Models/SmartContracts/TicketFeature.cs
@@ -2,5 +2,17 @@
 {
     public class TicketFeature
     {
+        public string RedeemCode { get; set; }
+        public int Quantity { get; set; }
+        public string EventName { get; set; }
+        public string EventDescription { get; set; }
+        public string EventLocation { get; set; } 
+        public DateTime EventDate { get; set; }
+        public string EventCode { get; set; }
+        public string EventWebsite { get; set; }
+        public bool IsRedeemed { get; set; }
+        public bool EvolveOnRedeem { get; set; }
+        public string SeatInfo { get; set; }
+        public DateTime? ExpireDate { get; set; }
     }
 }
