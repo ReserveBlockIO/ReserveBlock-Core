817372bf9803f2bb47c51f7438113789fd5beb07
Mathis <20599614+mathis1337@users.noreply.github.com>
2022-07-02 20:07:48 -0500

    Adding action filter for call request override

---
+++ ReserveBlockCore/Controllers/ActionFilterController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ReserveBlockCore.Controllers
+{
+    public class ActionFilterController : ActionFilterAttribute 
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if(Program.AlwaysRequireAPIPassword == true)
+            {
+                var somepass = filterContext.RouteData.Values.ContainsKey("somePassword");
+                if (somepass)
+                {
+                    var pass = filterContext.RouteData.Values["somePassword"].ToString();
+                    var passCheck = Program.APIPassword.ToDecrypt(pass);
+                    if (passCheck == pass && passCheck != "Fail")
+                    {
+                        //Allow command to process
+                    }
+                    else
+                    {
+                        filterContext.Result = new StatusCodeResult(403);
+                    }
+                }
+                else
+                {
+                    filterContext.Result = new StatusCodeResult(403);
+                }
+            }
+        }
+    }
+}
