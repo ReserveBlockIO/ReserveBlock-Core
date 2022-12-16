b3da5c3738f5d78d41d572ec10876ff0b71b7fcb
AlexanderSWilliams <implementnap@gmail.com>
2022-12-16 14:57:14 -0600

    Clear cache at beginning of round

--- ReserveBlockCore/Services/ClientCallService.cs
+++ ReserveBlockCore/Services/ClientCallService.cs
@@ -1045,7 +1045,10 @@ namespace ReserveBlockCore.Services
         }
 
         public static void ClearRoundDicts(long height)
-        {            
+        {
+            ConsensusServer.Messages.Clear();
+            ConsensusServer.Hashes.Clear();
+
             foreach (var key in Globals.TaskSelectedNumbersV3.Keys)
                 if (key.Height <= height)
                     Globals.TaskSelectedNumbersV3.TryRemove(key, out _);
