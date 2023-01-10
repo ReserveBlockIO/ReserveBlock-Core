00421e0aa2873b2cf2b8d08fe2c0c44e6db4ceb0
AlexanderSWilliams <implementnap@gmail.com>
2023-01-10 14:31:02 -0600

    Update StartupP2P.cs

--- ReserveBlockCore/StartupP2P.cs
+++ ReserveBlockCore/StartupP2P.cs
@@ -63,7 +63,7 @@ namespace ReserveBlockCore
                     });
                 }
 
-                if(Globals.AdjudicateAccount == null)
+                if(Globals.LastBlock.Height > Globals.BlockLock || Globals.AdjudicateAccount == null)
                 {
                     endpoints.MapHub<P2PServer>("/blockchain", options => {
                         options.ApplicationMaxBufferSize = 8388608; // values might need tweaking if mem consumption gets too large
