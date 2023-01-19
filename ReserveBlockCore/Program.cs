c3aaa7c9079e25e9aff3179b52737040f1dc0f2a
Mathis1337 <20599614+mathis1337@users.noreply.github.com>
2023-01-19 13:40:03 -0600

    Block height lock finalized. Zenith release ready

--- ReserveBlockCore/Program.cs
+++ ReserveBlockCore/Program.cs
@@ -86,7 +86,7 @@ namespace ReserveBlockCore
             SeedNodeService.SeedNodes();
             SeedNodeService.SeedBench();
 
-            Globals.BlockLock = Globals.IsTestNet == true ? 16 : 578450;
+            Globals.BlockLock = Globals.IsTestNet == true ? 16 : 578573;
 
             //BlockchainData.InitializeChain();
 
