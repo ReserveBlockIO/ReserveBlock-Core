c562332c64c414ac40c2a7197ca883b90c266ccc
Mathis1337 <20599614+mathis1337@users.noreply.github.com>
2023-03-29 23:45:38 -0500

    API update for exchange, initial fee calcs and bases. Simplifying fee to a struct of 8

--- ReserveBlockCore/Services/FeeCalcService.cs
+++ ReserveBlockCore/Services/FeeCalcService.cs
@@ -13,9 +13,31 @@ namespace ReserveBlockCore.Services
 
             var txSize = JsonConvert.SerializeObject(tx).Length;
             
-            txFee = (txSize / kb) * baseFeeMultiplier; 
+            txFee = (txSize / kb) * baseFeeMultiplier;
 
+            //base fee of 0.000003 must always exist
+            if (txFee < 0.000003M)
+            {
+                txFee = 0.000003M;
+            }
+            else
+            {
+                //fees are now capped at 8 decimal places
+                txFee = decimal.Round(txFee, 8);
+            }
             return txFee;
+        }
+
+        public static decimal GetFeeMinimum()
+        {
+            if(Globals.LastBlock.Height > 9999999)
+            {
+                return 0.000003M;
+            }
+            else
+            {
+                return 0.0M;
+            }
         }
 
         public static void CalculateDataFee()
