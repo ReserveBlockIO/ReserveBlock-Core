0593966798284a3319e5fe331c938ce54578b76c
Mathis1337 <20599614+mathis1337@users.noreply.github.com>
2023-07-13 14:23:40 -0500

    Update TKV2Controller.cs

--- ReserveBlockCore/Controllers/TKV2Controller.cs
+++ ReserveBlockCore/Controllers/TKV2Controller.cs
@@ -624,7 +624,7 @@ namespace ReserveBlockCore.Controllers
         /// </summary>
         /// <param name="tUID"></param>
         /// <returns></returns>
-        [HttpGet("GetVotesByAddress/{tUID}")]
+        [HttpGet("GetTokenOwnerVoteList/{tUID}")]
         public async Task<string> GetTokenOwnerVoteList(string tUID)
         {
             var votes = TokenVote.GetOwnerVoteList(tUID);
