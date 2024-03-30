71c2e71ca02abfd3758db0ec2d1cfbef4b4da878
Mathis1337 <20599614+mathis1337@users.noreply.github.com>
2024-03-30 14:28:42 -0500

    Update TokenizationFeature.cs

--- ReserveBlockCore/Models/SmartContracts/TokenizationFeature.cs
+++ ReserveBlockCore/Models/SmartContracts/TokenizationFeature.cs
@@ -5,6 +5,6 @@
         public string AssetName { get; set; }
         public string AssetTicker { get; set; }
         public bool KeyRevealed { get; set; }
-        public string KeyRevealRequestHash { get; set; }
+        public string? KeyRevealRequestHash { get; set; }
     }
 }
